d5797c783dbe8843195fc13c9bb2efd1f665c79c
mikemouse <kmmyemail@ukr.net>
2023-04-06 21:59:29 +0300

    build: add dynamic data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,19 +2,27 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
 function App() {
+  const data = [
+    { name: 'John C.', salary: 800, increase: false },
+    { name: 'Alex M.', salary: 3000, increase: true },
+    { name: 'Carl W.', salary: 5000, increase: false },
+  ];
+
   return (
     <div className="app">
-      <AppInfo></AppInfo>
+      <AppInfo />
       <div className="search-panel">
-        <SearchPanel></SearchPanel>
-        <AppFilter></AppFilter>
+        <SearchPanel />
+        <AppFilter />
       </div>
 
-      <EmployeesList></EmployeesList>
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
     </div>
   );
 }
