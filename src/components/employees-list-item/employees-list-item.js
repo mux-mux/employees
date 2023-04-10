161dfcda246824b64d507bcf853b053e0f124f20
mikemouse <kmmyemail@ukr.net>
2023-04-10 21:59:55 +0300

    feat: add/remove users

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -13,7 +13,7 @@ class EmployeesListItem extends Component {
 
   onIncrease = () => {
     this.setState(({ increase }) => ({
-      increse: !increase,
+      increase: !increase,
     }));
   };
 
@@ -24,7 +24,7 @@ class EmployeesListItem extends Component {
   };
 
   render() {
-    const { name, salary } = this.props;
+    const { name, salary, onDelete } = this.props;
     const { increase, rise } = this.state;
     let classNames = 'list-group-item d-flex justify-content-between';
 
@@ -42,11 +42,11 @@ class EmployeesListItem extends Component {
         </span>
         <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
         <div className="d-flex justify-content-center align-items-center">
-          <button type="button" className="btn-cookie btn-sm " onClick={this.onIncrease}>
+          <button type="button" className="btn-cookie btn-sm" onClick={this.onIncrease}>
             <i className="fas fa-cookie"></i>
           </button>
 
-          <button type="button" className="btn-trash btn-sm ">
+          <button type="button" className="btn-trash btn-sm" onClick={onDelete}>
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
